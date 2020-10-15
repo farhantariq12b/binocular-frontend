615cc8758e7a1640e667e2a82bc3b983b0163946
farhantariq <farhantariq12b@gmail.com>
2020-10-15 15:15:30 +0500

    apex chart changed

--- pages/executivesummary/index.js
+++ pages/executivesummary/index.js
@@ -2,9 +2,9 @@ import Chart from "react-google-charts";
 import { PieChart } from 'react-minimal-pie-chart';
 import SideNav from '../../components/SideNav.js'
 import Navbar from '../../components/Navbar.js'
-import InventoryPlaning from '../inventoryplanning/index.js'
 import React from 'react';
 import axios from 'axios'; 
+import ReactApexChart from "react-apexcharts"
 
 
 
@@ -59,7 +59,13 @@ class Executive extends React.Component{
       console.log(err)
     })
    
+
   }
+
+
+
+
+
 
   componentDidMount() {
     // let ss_date = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
@@ -444,8 +450,80 @@ class Executive extends React.Component{
         yperrev  = ((yearlyrev/100)*lastyearlyrev)
         let yperunit  = 0
         yperunit  = ((yearlyunit/100)*lastyearlyunit)
-      
+        
 
+        ///////chart configs
+        let series = [{
+          name: 'TEAM A',
+          type: 'column',
+          data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]
+        }, {
+          name: 'TEAM B',
+          type: 'area',
+          data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
+        }, {
+          name: 'TEAM C',
+          type: 'line',
+          data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
+        }]
+         let options= {
+          chart: {
+            height: 350,
+            type: 'line',
+            stacked: false,
+          },
+          stroke: {
+            width: [0, 2, 5],
+            curve: 'smooth'
+          },
+          plotOptions: {
+            bar: {
+              columnWidth: '50%'
+            }
+          },
+          
+          fill: {
+            opacity: [0.85, 0.25, 1],
+            gradient: {
+              inverseColors: false,
+              shade: 'light',
+              type: "vertical",
+              opacityFrom: 0.85,
+              opacityTo: 0.55,
+              stops: [0, 100, 100, 100]
+            }
+          },
+          labels: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003',
+            '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'
+          ],
+          markers: {
+            size: 0
+          },
+          xaxis: {
+            type: 'datetime'
+          },
+          yaxis: {
+            title: {
+              text: 'Points',
+            },
+            min: 0
+          },
+          tooltip: {
+            shared: true,
+            intersect: false,
+            y: {
+              formatter: function (y) {
+                if (typeof y !== "undefined") {
+                  return y.toFixed(0) + " points";
+                }
+                return y;
+          
+              }
+            }
+          }
+        };
+  
+ 
 
   return (
   <div className = "main-wrapper">
@@ -626,7 +704,7 @@ class Executive extends React.Component{
 
           {/* <img src="cart-big.png" alt="Cart"></img> */}
 
-
+{/* 
           <Chart
             width={'100%'}
             height={'350'}
@@ -656,7 +734,7 @@ class Executive extends React.Component{
               },
             }}
             rootProps={{ 'data-testid': '4' }}
-          />
+          /> */}
         {/* 
           <Chart
 
@@ -685,7 +763,8 @@ class Executive extends React.Component{
               }}
               rootProps={{ 'data-testid': '2' }}
             /> */}
-                      
+          <ReactApexChart options={options} series={series} type="line" height={450} />
+    
           </div>
           
         </div>
